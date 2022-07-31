098e8c8d0876704029ea9e5ebfd6ca132287876b
Vithu Shanth <vithuren.27@gmail.com>
2022-07-31 16:11:56 -0400

    Created Azure Function app

--- frontend/main.js
+++ frontend/main.js
@@ -2,20 +2,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://vithurengetresumecounter.azurewebsites.net/api/{x:regex(^(warmup|csharphttpwarmup)$)}?';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
-const getVisitCount = () => {
+function getVisitCount() {
     let count = 30;
-    fetch(functionApi)
-    .then(response => {
-        return response.json()
-    })
-    .then(response => {
-        console.log("Website called function API.");
-        count = response.count;
-        document.getElementById('counter').innerText = count;
-    }).catch(function(error) {
-        console.log(error);
-      });
+    fetch(functionApiUrl).then(response => {
+        return response.json();
+    }).then(response => {
+            console.log("Website called function API.");
+            count = response.count;
+            document.getElementById('counter').innerText = count;
+        }).catch(function(error){
+            console.log(error);
+        });
     return count;
 }
